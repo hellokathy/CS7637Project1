416490f276f756bb0402ec9e23790589d0de4f05
jwall9108 <jwall9108@gmail.com>
2014-08-22 17:37:52 -0400

    update

--- src/project1/Agent.java
+++ src/project1/Agent.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Map.Entry;
-
 
 /**
  * Your Agent for solving Raven's Progressive Matrices. You MUST modify this
@@ -31,11 +31,11 @@ public class Agent {
 	 * main().
 	 * 
 	 */
-	//used to test an assumptions strength 
-	//i.e. if a transformation has a strong similarity weight than the
-	//AI can make tolerance levels to arrive at the best answer
+	// used to test an assumptions strength
+	// i.e. if a transformation has a strong similarity weight than the
+	// AI can make tolerance levels to arrive at the best answer
 	public HashMap<String, Integer> Weights;
-	
+
 	public Agent() {
 		Weights = new HashMap<String, Integer>();
 		Weights.put("Unchanged", 5);
@@ -72,119 +72,135 @@ public class Agent {
 	 * @return your Agent's answer to this problem
 	 */
 	public String Solve(RavensProblem problem) {
-		
-		//seperate problems from possible choices
+
+		// seperate problems from possible choices
 		System.out.println(problem.getName());
 		HashMap<String, RavensFigure> choices = getChoices(problem.getFigures());
-		HashMap<String, RavensFigure> problems = getProblems(problem.getFigures());
-		getTranformStrength(problems.get("A").getObjects(), problems.get("B").getObjects());
-		System.out.println("---------------------------------------------------------");
+		HashMap<String, RavensFigure> problems = getProblems(problem
+				.getFigures());
+		getTranformStrength(problems.get("A").getObjects(), problems.get("B")
+				.getObjects());
+		System.out
+				.println("---------------------------------------------------------");
+		
+		
 		return "1";
 	}
-
+	public String deriveBestChoice(RavensFigure problem, HashMap<String, RavensFigure> choices) {
+		HashMap<RavensFigure, Integer> choiceStrength = new HashMap<RavensFigure, Integer>();
+		
+		return null;
+	
+	}
+	public HashMap<RavensFigure, Integer> buildStrengthMap(HashMap<String, RavensFigure> choices) {
+		
+		
+		return null;
+		
+	}
 	public HashMap<String, RavensFigure> getChoices(
 			HashMap<String, RavensFigure> figures) {
-		
+		HashMap<RavensFigure, Integer> possible = new HashMap<>();
 		HashMap<String, RavensFigure> list = new HashMap<String, RavensFigure>();
 		Iterator<Entry<String, RavensFigure>> it = figures.entrySet()
 				.iterator();
 		while (it.hasNext()) {
 			Map.Entry<String, RavensFigure> pairs = (Map.Entry<String, RavensFigure>) it
 					.next();
-			//System.out.println(pairs.getKey() + " = " + pairs.getValue());
+			// System.out.println(pairs.getKey() + " = " + pairs.getValue());
 			if (pairs.getKey().toString().matches("-?\\d+(\\.\\d+)?")) {
-				list.put(pairs.getKey().toString(), (RavensFigure) pairs.getValue());
+				list.put(pairs.getKey().toString(),
+						(RavensFigure) pairs.getValue());
+				possible.put((RavensFigure) pairs.getValue(), randInt(0, 20));
 			}
-			//it.remove(); // avoids a ConcurrentModificationException
+			// it.remove(); // avoids a ConcurrentModificationException
 		}
+
+		List<String> sorted = getOrderByStrength(possible);
+
 		return list;
 	}
-	
+
 	
 	public HashMap<String, RavensFigure> getProblems(
 			HashMap<String, RavensFigure> figures) {
-		
+
 		HashMap<String, RavensFigure> list = new HashMap<String, RavensFigure>();
 		Iterator<Entry<String, RavensFigure>> it = figures.entrySet()
 				.iterator();
 		while (it.hasNext()) {
 			Map.Entry<String, RavensFigure> pairs = (Map.Entry<String, RavensFigure>) it
 					.next();
-			//System.out.println(pairs.getKey() + " = " + pairs.getValue());
+			// System.out.println(pairs.getKey() + " = " + pairs.getValue());
 			if (!pairs.getKey().toString().matches("-?\\d+(\\.\\d+)?")) {
-				list.put(pairs.getKey().toString(), (RavensFigure) pairs.getValue());
+				list.put(pairs.getKey().toString(),
+						(RavensFigure) pairs.getValue());
 			}
-			//it.remove(); // avoids a ConcurrentModificationException
+			// it.remove(); // avoids a ConcurrentModificationException
 		}
 		return list;
 	}
-	
-	public int getTranformStrength(ArrayList<RavensObject> a, ArrayList<RavensObject> b) {
+
+	public int getTranformStrength(ArrayList<RavensObject> a,
+			ArrayList<RavensObject> b) {
 		for (RavensObject obj : a) {
 			for (RavensObject object : b) {
-				if(object.getName().equals(obj.getName())){
-					System.out.println(obj.getName() + ":" +object.getName());
-					compareAttributes(obj.getAttributes(), object.getAttributes());
-					
+				if (object.getName().equals(obj.getName())) {
+					System.out.println(obj.getName() + ":" + object.getName());
+					compareAttributes(obj.getAttributes(),
+							object.getAttributes());
 				}
 			}
 		}
 		int strength = 0;
 		return strength;
-		
-	}
-	
-	
-	public int compareAttributes(ArrayList<RavensAttribute> a, ArrayList<RavensAttribute> b) {
-		for (RavensAttribute obj : a) {
-			for (RavensAttribute object : b) {
-				if(object.getName().equals(obj.getName())){
-					System.out.println(obj.getName() + ":" +obj.getValue());
-					System.out.println(object.getName() + ":" + object.getValue());
-				}
-			}
-		}
-		int strength = 0;
-		return strength;
-		
-	}
-	
-	public boolean isRotation() {
-		return false;
-	}
-	public boolean isDeletion() {
-		return false;
-	}
-	public boolean isReflection() {
-		return false;
-	}
-	public boolean isUnchanged() {
-		return false;
-	}
-	public boolean isScaled() {
-		return false;	
-	}
-	public boolean isShapeChanged() {
-		return false;
 	}
 
-//	public int getOrderByStrength(String studentName) {
-//		List<String> studentsList = new ArrayList<String>();
-//		List<Entry<Student, Double>> list = new ArrayList<Entry<Student, Double>>(
-//				getAllGrades().entrySet());
-//		Collections.sort(list, new Comparator<Map.Entry<Student, Double>>() {
-//			public int compare(Map.Entry<Student, Double> o1,
-//					Map.Entry<Student, Double> o2) {
-//				return (o2.getValue()).compareTo(o1.getValue());
-//			}
-//		});
-//		for (Map.Entry<Student, Double> entry : list) {
-//			studentsList.add(entry.getKey().getName());
-//		}
-//		return studentsList.indexOf(studentName) + 1;
-//	}
-	
+	public int compareAttributes(ArrayList<RavensAttribute> a,
+			ArrayList<RavensAttribute> b) {
+		for (RavensAttribute obj : a) {
+			for (RavensAttribute object : b) {
+				if (object.getName().equals(obj.getName())) {
+					System.out.println(obj.getName() + ":" + obj.getValue());
+					System.out.println(object.getName() + ":"
+							+ object.getValue());
+				}
+			}
+		}
+		int strength = 0;
+		return strength;
+
+	}
+
+	public String getAttributeValue(RavensObject a, String key) {
+		for (RavensAttribute obj : a.getAttributes()) {
+			if (obj.getName().equals(key.toLowerCase())) {
+				return obj.getValue();
+			}
+		}
+		return null;
+	}
+
+	public List<String> getOrderByStrength(
+			HashMap<RavensFigure, Integer> choices) {
+		List<String> choiceList = new ArrayList<String>();
+		List<Entry<RavensFigure, Integer>> list = new ArrayList<Entry<RavensFigure, Integer>>(
+				choices.entrySet());
+		Collections.sort(list,
+				new Comparator<Map.Entry<RavensFigure, Integer>>() {
+					public int compare(Map.Entry<RavensFigure, Integer> o1,
+							Map.Entry<RavensFigure, Integer> o2) {
+						return (o2.getValue()).compareTo(o1.getValue());
+					}
+				});
+		for (Map.Entry<RavensFigure, Integer> entry : list) {
+			choiceList.add(entry.getKey().getName());
+		}
+		return choiceList;
+	}
+
 	public int getSides(String shape) {
+
 		switch (shape) {
 		case "triangle":
 			return 3;
@@ -206,7 +222,38 @@ public class Agent {
 			break;
 		}
 		return 0;
-		
+
 	}
 
+	public static int randInt(int min, int max) {
+
+		Random rand = new Random();
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+
+	}
+
+	public boolean isRotation() {
+		return false;
+	}
+
+	public boolean isDeletion() {
+		return false;
+	}
+
+	public boolean isReflection() {
+		return false;
+	}
+
+	public boolean isUnchanged() {
+		return false;
+	}
+
+	public boolean isScaled() {
+		return false;
+	}
+
+	public boolean isShapeChanged() {
+		return false;
+	}
 }
